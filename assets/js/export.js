36bf691689a93ff88aa1fffaef6dca6330a84bef
joshuk <25204957+joshuk@users.noreply.github.com>
2020-12-16 23:33:48 +0000

    Finish building HTML in export

--- assets/js/export.js
+++ assets/js/export.js
@@ -6,6 +6,10 @@ $('.export').addEventListener('click', () => {
 
     //Right, now start the export process
     sortImmediateChildren();
+    
+    //Now we've done that, we can start to
+    //loop through and generate the html
+    generateEmail();
 });
 
 const sortImmediateChildren = () => {
@@ -113,6 +117,12 @@ const sortChildrenByY = (children) => {
     });
 }
 
+const sortChildrenByX = (children) => {
+    return children.sort(function (a, b) {
+        return boxes[a].startX - boxes[b].startX;
+    });
+}
+
 const mergeObjectToParent = (object) => {
     //This function is only used in the first step
     //to merge two arrays together. I can't use
@@ -120,8 +130,8 @@ const mergeObjectToParent = (object) => {
 
     let keys = Object.keys(object);
 
-    for(let j=0;j<keys.length;j++){
-        let currentBoxID = keys[j];
+    for(let i=0;i<keys.length;i++){
+        let currentBoxID = keys[i];
         let currentBox = object[currentBoxID];
 
         boxes[currentBoxID] = currentBox;
@@ -130,4 +140,138 @@ const mergeObjectToParent = (object) => {
             boxes[getParentBoxID()].children.push(currentBoxID);
         }
     }
+}
+
+const generateEmail = () => {
+    //We're gonna do this again
+    const parentBox = getParentBox();
+    const parentChildren = parentBox.children;
+    const sortedChildren = sortChildrenByY(parentChildren);
+
+    let emailElement = createElement('table');
+
+    for(let i=0;i<sortedChildren.length;i++){
+        let currentBox = sortedChildren[i];
+
+        let currentBoxHTML = generateHTMLFromBox(currentBox, emailElement);
+        emailElement.childNodes[0].appendChild(currentBoxHTML);
+    }
+
+    console.log(boxes);
+    console.log(emailElement);
+}
+
+const generateHTMLFromBox = (id, parentElement) => {
+    const box = boxes[id];
+    let newCell;
+
+    switch(box.type){
+        case 'container':
+            //These variables are up here to stop JS
+            //shouting at me that they're undefined.
+            //They aren't used if the parent isn't a
+            //table.
+            let newRow;
+
+            if(parentElement.tagName == 'TABLE'){
+                //Then this is the parent box, so
+                //create some elements for the children
+                //to sit in
+                newRow = parentElement.insertRow();
+                newCell = newRow.insertCell();
+            }
+
+            let newTable = createElement('table');
+
+            //Check here whether this container has several
+            //rows as children (warning: beef)
+            if(box.children.length > 1 
+            && boxes[box.children[0]].width == box.width && boxes[box.children[0]].height != box.height){
+                //This is a group of trs
+                let boxChildren = sortChildrenByY(box.children);
+
+                for(let i=0;i<boxChildren.length;i++){
+                    let currentChild = boxChildren[i];
+
+                    let newTableRow = newTable.insertRow();
+                    newTableRow.appendChild(generateHTMLFromBox(currentChild, newTableRow));
+                }
+            }else{
+                //This is a group of tds
+                let newTableRow = newTable.insertRow();
+
+                let boxChildren = sortChildrenByX(box.children);
+
+                for(let i=0;i<boxChildren.length;i++){
+                    let currentChild = boxChildren[i];
+    
+                    //let newTableCell = newTableRow.insertCell();
+                    newTableRow.appendChild(generateHTMLFromBox(currentChild, newTableRow));
+                }
+            }
+
+            if(parentElement.tagName == 'TABLE'){
+                newCell.appendChild(newTable);
+            }
+
+            return (parentElement.tagName == 'TABLE' ? newRow : newTable);
+        break;
+        case 'child':
+            newCell = createElement('td');
+
+            if(box.children.length){
+                for(let i=0;i<box.children.length;i++){
+                    let currentChild = box.children[i];
+
+                    newCell.appendChild(generateHTMLFromBox(currentChild, newCell));
+                }
+            }else{
+                let newLink;
+
+                if(box.url !== undefined){
+                    console.log('create a new link ' + id);
+
+                    newLink = createElement('a');
+                    newLink.href = box.url;
+                    newLink.target = '_blank';
+                }
+
+                //This is the last child, so we need to get the content
+                let newText = document.createTextNode(`image ${id} - alt (${(box.alt ?? '\'\'')})`);
+
+                if(box.url !== undefined){
+                    console.log('add new link ' + id);
+                    newLink.appendChild(newText);
+                    newCell.appendChild(newLink);
+                }else{
+                    console.log('no link ' + id);
+                    newCell.appendChild(newText);
+                }
+            }
+
+            return newCell;
+        break;
+    }
+}
+
+const createElement = (tag, { childNodes, innerHTML, textContent, attrs } = {}) => {
+    let element = document.createElement(tag);
+
+    if(childNodes) {
+        Array.from(childNodes).forEach(element.appendChild);
+    }
+
+    if(innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+
+    if(textContent) {
+        element.textContent = textContent;
+    }
+
+    if(attrs) {
+        Object.keys(attrs).forEach(attr => element.setAttribute(attr, attrs[attr]));
+    }
+
+    return element;
 }
